e911b9bec0eb198126664f7be56fe646c9c400a8
Daniel Scholz <daniel.martin.scholz@gmail.com>
2018-11-24 19:56:21 -0800

    added exercises table with user references

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_032617) do
+ActiveRecord::Schema.define(version: 2018_11_25_034757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_exercises_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -26,4 +35,5 @@ ActiveRecord::Schema.define(version: 2018_11_25_032617) do
     t.string "username"
   end
 
+  add_foreign_key "exercises", "users"
 end
