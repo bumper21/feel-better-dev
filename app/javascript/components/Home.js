ab19adfcb176dc41d6b9a9f504f9c547827bd226
Daniel Scholz <daniel.martin.scholz@gmail.com>
2018-11-29 12:35:24 -0800

    home.js

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -2,17 +2,18 @@ import React from 'react';
 // import { Player } from 'video-react';
 
 class Home extends React.Component {
+  handleSubmit
   render () {
     return (
-        <div className="container flex-grow" name="buttonContainer">
+        <div className="container" name="buttonContainer">
           {/* <Player>
             <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
           </Player> */}
-          <button className="btn" data-toggle="modal">Watch Better</button>
+          <button className="btn" name="VidModal" data-toggle="modal">Watch Better</button>
           <br />
-          <button className="btn btn-warning m-1">Journal Better</button>
+          <button className="btn btn-warning m-1" name="JournalModal">Journal Better</button>
           <br />
-          <button className="btn btn-success m-1">Feel Better</button>
+          <button className="btn btn-success m-1" name="ExerciseModal">Feel Better</button>
         </div>
     )
   }
