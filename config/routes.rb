364eaa8f7c5b3abffcc73e337290345775c3f794
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-01-14 23:42:01 -0800

    view and controller created with brief site description

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
+  get '/about', to: 'about#index'
+
+  
   
   resources :users, only: [:new, :create, :show]
   resource :sessions, only: [:new, :create, :destroy]
   resources :exercises
   resources :journal_entries, only: [:new, :create, :destroy, :index]
 
+  # React API
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :exercises, only: [ :index, :show, :create, :destroy, :show_random ]
