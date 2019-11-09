38a32dba70da271cdfa526293e57382d677cb062
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-11-08 21:07:52 -0800

    username validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-
   describe ".full_name" do
     it "returns the first and last name concatenated" do
       u = User.new({first_name: "John", last_name: "Smith"})
       expect(u.full_name).to eq("John Smith")
+    end
+  end
+
+  describe ".username" do
+    it "requires username" do
+      u = User.new
+      u.valid?
+      puts u.errors.messages
+      expect(u.errors.messages).to have_key(:username)
     end
   end
 end
