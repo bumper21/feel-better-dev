bcc27f1e80946e2627719396387e925596073dff
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-11-18 23:24:59 -0800

    skip before action removed api journal entry

--- app/controllers/api/v1/journal_entries_controller.rb
+++ app/controllers/api/v1/journal_entries_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::JournalEntriesController < Api::ApplicationController
-  skip_before_action :verify_authenticity_token
 
   def index
     journal_entries = JournalEntry.order created_at: :desc
