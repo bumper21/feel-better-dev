1318554881fc2546e0d27c4565d6b9cb619211f5
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-11-18 20:53:49 -0800

    test for base_url set to production url

--- app/javascript/packs/requests.js
+++ app/javascript/packs/requests.js
@@ -1,4 +1,5 @@
-const BASE_URL = `http://localhost:3000/api/v1`;
+// const BASE_URL = `http://localhost:3000/api/v1`;
+const BASE_URL = `https://feel--better.herokuapp.com/`;
 
 
 export const Exercise = {
