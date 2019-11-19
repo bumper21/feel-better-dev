0d8b95860450e671e8b103f94cef0089257d86a5
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-11-18 23:33:26 -0800

    protect from forgery added to controller

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,5 +1,5 @@
 class Api::ApplicationController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  protect_from_forgery prepend: true
 
   private
   def authenticate_user!
