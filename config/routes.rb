490e1c8348d73b27df1427a5ad1ea1ab73dd6710
Daniel Scholz <daniel.martin.scholz@gmail.com>
2019-11-18 22:55:24 -0800

    removed forward slash from exercise routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :exercises, only: [ :index, :show, :create, :destroy, :show_random ]
-      get '/exercise/random', to: 'exercises#show_random'
+      get 'exercise/random', to: 'exercises#show_random'
       resources :journal_entries, only: [ :create ]
     end
   end
